96e5179b84f9ec8d1c9217fee87bc87e7f47fed8
GeneralGaws <122978178+GeneralGaws@users.noreply.github.com>
2024-12-04 01:57:37 +0300

    Variant (#4)
    * fine
    
    * add
    
    * ja
    
    * adaptive+script
    
    * fix
    
    * up (#3)
    
    * Variant (#2)
    
    * fine
    
    * add
    
    * ja
    
    ---------
    
    Co-authored-by: GeneralGaws <limonmessi@mail.ru>
    
    * Create README.MD
    
    ---------
    
    Co-authored-by: GeneralGaws <limonmessi@mail.ru>
    
    ---------
    
    Co-authored-by: GeneralGaws <limonmessi@mail.ru>

--- randomPhrases.js
+++ randomPhrases.js
@@ -1,18 +1,24 @@
 const inspirationalQuotes = [
   'Сегодня просто БОМБИЧЕСКИЙ день',
-  'Весь мир - станция NT, а мы в ней - ядерные боеголовки',
+  'Весь мир - космическая станция, а мы в ней - ядерные боеголовки',
   'Не бойся, больно не будет',
   'Вырезаем экипаж, достаём диск и забываем код',
   'Ни дня без военных преступлений',
 ];
 
 const replaceQuotes = document.querySelector('.phrase-text');
+const diffTextBlock = document.getElementById('job-diff');
 
 window.onload = () => {
-  // Сохранить длину массива как переменную
+//Рандом цитата //
   let inspirationalQuotesSize = inspirationalQuotes.length;
-  // Генерируем рандомное число от 0 до количество записей в массиве
   let randomQuote = Math.floor(Math.random() * inspirationalQuotesSize);
-  // Добавляем фразу в нам элемент ui_txt02
   replaceQuotes.textContent = inspirationalQuotes[randomQuote];
-}
+  
+//Цвет под сложность роли
+  var diffText = diffTextBlock.innerText;
+  if (diffText == 'Тяжело') {diffTextBlock.style.color = 'red';}
+  else if (diffText == 'Очень тяжело') {diffTextBlock.style.color = 'darkred';}
+  else if (diffText == 'Легко') {diffTextBlock.style.color = 'darkgreen';}
+  else if (diffText == 'Средне') {diffTextBlock.style.color = 'darkyellow';}
+};
