7f2d7f91c2994cb47fb2d44f35e57fc950523438
GeneralGaws <limonmessi@mail.ru>
2024-12-05 01:25:44 +0300

    adapt

--- randomPhrases.js
+++ randomPhrases.js
@@ -8,6 +8,9 @@ const inspirationalQuotes = [
 
 const replaceQuotes = document.querySelector('.phrase-text');
 const diffTextBlock = document.getElementById('job-diff');
+const ContentBlock = document.querySelector('sidebar-container');
+const h3Block = document.querySelector('h3');
+const idMassive = [ document.querySelectorAll('h3') ];
 
 window.onload = () => {
 //Рандом цитата //
@@ -21,4 +24,18 @@ window.onload = () => {
   else if (diffText == 'Очень тяжело') {diffTextBlock.style.color = 'darkred';}
   else if (diffText == 'Легко') {diffTextBlock.style.color = 'darkgreen';}
   else if (diffText == 'Средне') {diffTextBlock.style.color = 'darkyellow';}
+
+//Автозаполнение содержания
+  var h3Text = h3Block.innerText;
+  var hlink = h3Block.getAttribute('id');
+  var hlinkID = hlink.innerHTML;
+    var innerText = "";
+    for (var j = 0; j < 10; j++) {
+      innerText += `<a class="sidebar-text" href="#${hlink}">${h3Text}</div>`;
+    }
+  console.log(h3Text);
+  console.log(hlink);
+  console.log(hlinkID);
+  console.log(idMassive);
+
 };
